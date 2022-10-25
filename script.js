2de6f3e00c041052f6f2e1f36f959c6750041310
niksonstha <sthanikson10@gmail.com>
2022-10-25 09:17:26 +0545

    added edit and delete button in task list in create branch

--- script.js
+++ script.js
@@ -6,11 +6,29 @@ addTask.addEventListener("click", (e) => {
   e.preventDefault();
   if (input.value != "") {
     console.log(input.value);
-    input.value = "";
 
     let task = document.createElement("li");
     addedTask.appendChild(task);
-    
+
+    let para = document.createElement("p");
+    task.appendChild(para);
+    para.innerHTML = input.value;
+
+    let buttonDiv = document.createElement("div");
+    buttonDiv.setAttribute("id", "edit__delete");
+    task.appendChild(buttonDiv);
+
+    let edit = document.createElement("button");
+    edit.innerText = "Edit";
+    edit.setAttribute("id", "edit");
+    buttonDiv.appendChild(edit);
+
+    let remove = document.createElement("button");
+    remove.innerText = "Delete";
+    remove.setAttribute("id", "delete");
+    buttonDiv.appendChild(remove);
+
+    input.value = "";
   } else {
     alert("please enter your task");
   }
